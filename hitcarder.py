328004d5d67bbb043840a212b40448f49fcfcbc2
fatcatmat <36107894+fatcatmat1994@users.noreply.github.com>
2022-05-11 11:51:36 +0800

    Update hitcarder.py

--- hitcarder.py
+++ hitcarder.py
@@ -9,7 +9,7 @@ import datetime
 import os
 import sys
 import message
-
+import ddddocr
 
 class HitCarder(object):
     """Hit carder class
@@ -29,6 +29,7 @@ class HitCarder(object):
         self.login_url = "https://zjuam.zju.edu.cn/cas/login?service=https%3A%2F%2Fhealthreport.zju.edu.cn%2Fa_zju%2Fapi%2Fsso%2Findex%3Fredirect%3Dhttps%253A%252F%252Fhealthreport.zju.edu.cn%252Fncov%252Fwap%252Fdefault%252Findex"
         self.base_url = "https://healthreport.zju.edu.cn/ncov/wap/default/index"
         self.save_url = "https://healthreport.zju.edu.cn/ncov/wap/default/save"
+        self.CAPTCHA_URL = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
         self.sess = requests.Session()
         self.sess.keep_alive = False
         retry = Retry(connect=3, backoff_factor=0.5)
@@ -76,7 +77,14 @@ class HitCarder(object):
         """Get current date."""
         today = datetime.datetime.utcnow() + datetime.timedelta(hours=+8)
         return "%4d%02d%02d" % (today.year, today.month, today.day)
-
+    
+    def get_captcha(self):
+        """Get CAPTCHA code"""
+        resp = self.sess.get(self.CAPTCHA_URL)
+        captcha = self.ocr.classification(resp.content)
+        print("验证码：", captcha)
+        return captcha
+    
     def check_form(self):
         """Get hitcard form, compare with old form """
         res = self.sess.get(self.base_url)
@@ -140,7 +148,8 @@ class HitCarder(object):
         new_info['sfymqjczrj'] = old_info['sfymqjczrj'] # 入境
         new_info['sfqrxxss'] = 1 # 属实
         new_info['campus'] = old_info['campus']
-
+        new_info['verifyCode'] = self.get_captcha()
+        
         self.info = new_info
         # print(json.dumps(self.info))
         return new_info
